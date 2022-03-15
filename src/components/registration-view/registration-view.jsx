6f8dccb5b3f7cf1b5594fd63d72637cb433362ee
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-03-15 17:07:50 -0400

    changes and such

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import  propTypes  from "prop-types";
+import { Form } from 'react-bootstrap';
+
+export function RegisterView(props) {
+    const [username, setUsername] = userState('');
+    const [password, setPassword] = userState('');
+    const [email, setEmail] = userState('');
+    const [birthday, setBirthday] = userState('');
+
+
+    const handleSubmit = (e) => {
+     e.preventDefault();
+        console.log(username, password, email, birthday);
+    //Send a request to the server for authentication
+    //Then call props.onRegistration(username);
+        props.onRegistration(username);
+    };
+
+    return (
+        <form>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <label>
+                Email:
+                <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+            </label>
+            
+            <div class="coc-form">
+                <div className="coc-block-row"></div>
+            </div>
+        </form>
+    )
+ }
+    
