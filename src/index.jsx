a0345927d24fb4b5d27df2b28a4c7e02ed00fbd4
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-04-06 13:35:11 -0400

    Added actions and reducers

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import ReactDOM from 'react-dom';
-import Mainview from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers.js';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import MainView from './components/main-view/main-view';
+
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
-  constructor() {
-    super();
-  }
   render() {
     return (
-      <Container id="main-view">
-        <Mainview />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
